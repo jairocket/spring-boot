26245a61da7b75a2c280adac21a93c96e4a5621a
Jailson Anjos <jailsonanjos@hotmail.com>
2023-11-23 10:04:03 -0300

    feat(order): Add OrderStatus

--- src/main/java/com/educando/web/course/entities/Order.java
+++ src/main/java/com/educando/web/course/entities/Order.java
@@ -1,11 +1,14 @@
 package com.educando.web.course.entities;
 
+import com.educando.web.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
+
+import static com.educando.web.course.entities.enums.OrderStatus.valueOf;
 
 @Entity
 @Table(name = "tb_order")
@@ -17,16 +20,19 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    private Integer orderStatus;
+
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, User client, OrderStatus orderStatus) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -51,6 +57,16 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null ){
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     @Override
