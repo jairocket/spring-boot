5ab78c431b21029a6937b994d0834eacc29a2bff
Jailson Anjos <jailsonanjos@hotmail.com>
2023-11-26 11:55:16 -0300

    feat(user): Handling update exception

--- src/main/java/com/educando/web/course/services/UserService.java
+++ src/main/java/com/educando/web/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.educando.web.course.entities.User;
 import com.educando.web.course.repositories.UserRepository;
 import com.educando.web.course.services.exceptions.DatabaseException;
 import com.educando.web.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -39,9 +40,14 @@ public class UserService {
     }
 
     public User update(Long id, User user){
-        User entity = repository.getReferenceById(id);
-        updateData(entity, user);
-        return repository.save(entity);
+        try{
+            User entity = repository.getReferenceById(id);
+            updateData(entity, user);
+            return repository.save(entity);
+        } catch (EntityNotFoundException exception) {
+            throw new ResourceNotFoundException(id);
+        }
+
     }
 
     private void updateData(User entity, User user) {
